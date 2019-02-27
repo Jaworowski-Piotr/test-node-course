3006cafca290c6111bad6bc626dd47991870f1ab
Jaworowski <jaworowski.piotr.marek@gmail.com>
2019-02-27 14:34:34 +0100

    Add a few utils test

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,46 @@
-const utils = require('./utils')
+const utils = require('./utils');
+const expect = require('expect');
 
-it('Should add two numbers', () => {
-    let res = utils.add(33,11);
 
-    if(res !== 44){
-        throw new Error(`Expect 44, but got ${res}`);
-    }
+it('Add two numbers', () => {
+    let res = utils.add(33, 11);
+
+    expect(res).toBe(44).toBeA('number');
 });
 
 it('Should square number', () => {
-    let res = utils.square(4);
+    let res = utils.square(3);
 
-    if(res !== 16){
-        throw new Error(`Expect 9, but got ${res}`);
+    expect(res).toBe(9).toBeA('number');
+});
+
+it('Should expect two values', () => {
+    // expect(12).toNotBe(11);
+    // expect({
+    //     name: "Piotr"
+    // }).toEqual({
+    //     name: "Piotr"
+    // })
+    expect([2,3,4]).toInclude(3);
+});
+
+it('Compare to var', () => {
+    expect({
+        name: "Piotr",
+        age: 23,
+        hobby: [
+            "sport"
+        ]
+    }).toInclude({name: "Piotr"})
+});
+
+it("should verify frist and last name are set", () => {
+    let user = {
+        location: "Warszawa"
     }
+
+    let secondUser = utils.setName(user, "Adam Nowak");
+
+    expect(secondUser.fristName).toBeA('string').toBe('Adam');
+    expect(secondUser.lastName).toBeA('string').toBe("Nowak");
 });
